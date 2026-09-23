aafe3979eca28aa2463633f5b42af11f739a2a57
Amino Belyamani <museicist@gmail.com>
2026-09-23 10:34:21 -0400

    Move validateEnv from /tools to main runtime entry

--- packages/astro/src/runtime/index.ts
+++ packages/astro/src/runtime/index.ts
@@ -9,6 +9,7 @@
 //#region -------------------------------------------------- Type Imports
 
 import type { HTMLAttributes } from "astro/types"
+import type { z } from "zod"
 import type { ValidAppComponent } from "./app-component/types.js"
 import type { ARGS_AppRouterClass, ComponentsRouterShape } from "./app-router/types.js"
 import type { I_BaseComponent } from "./types/base-component.js"
@@ -319,6 +320,39 @@ export function makeURLSearchParams<T extends Record<string, unknown>>(
 	options?: string[][] | Record<string, string> | string | URLSearchParams
 ): I_URLSearchParams<T> {
 	return new URLSearchParams(options) as unknown as I_URLSearchParams<T>
+}
+
+/*******************************************************************************
+ *
+ * Helper to validate and provide type safety and intellisense for your environment variables.
+ *
+ * @typeParam Env - The generated env types from `env-types.ts`.
+ *
+ * @param {unknown} env The env object from `process.env`.
+ * @param {z.ZodType} zodSchema The generated zod schema from `env-schema.ts`.
+ *
+ * @returns {Readonly<Env>} The fully typed env.
+ * @throws {Error} If the passed environment does not match the schema.
+ *
+ * @example
+ * ```ts
+ * import type { HAQ_ENV } from "./env-types.js"
+ *
+ * import { envSchema } from "./env-schema.js"
+ * import { validateEnv } from "@haq/astro"
+ *
+ * export const ENV = validateEnv<HAQ_ENV>(process.env, envSchema)
+ * // now you can use it safely anywhere in your project
+ * ```
+ *
+ * @author Amino Belyamani
+ ******************************************************************************/
+
+export function validateEnv<Env extends Record<string, string>>(env: unknown, zodSchema: z.ZodType): Readonly<Env> {
+	const ENV = zodSchema.safeParse(env)
+	if (ENV.error) throw new Error(ENV.error.message)
+
+	return Object.freeze(ENV.data) as Readonly<Env>
 }
 
 //#endregion ----------------------------------------------- Useful API
--- packages/astro/src/tools/index.ts
+++ packages/astro/src/tools/index.ts
@@ -8,7 +8,6 @@
 
 // biome-ignore lint/performance/noBarrelFile: These exposed functions are almost always used together so no significant tree shaking effect.
 export { GLOBALS } from "../globals.js"
-export { validateEnv } from "./env.js"
 export type { I_LSPTools } from "./lsp/index.js"
 export { makeLspTools } from "./lsp/index.js"
 export { removeAstroAttributes } from "./remove-attributes.js"
